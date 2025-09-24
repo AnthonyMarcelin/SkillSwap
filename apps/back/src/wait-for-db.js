ce5384f91aec94538a26f579b35594b351f78154
AnthonyMarcelin <anthony.marcelin13@gmail.com>
2025-09-24 11:53:14 +0200

    chore(docker): add Dockerfiles and compose for local deployment

---
+++ apps/back/src/wait-for-db.js
@@ -0,0 +1,26 @@
+const { Client } = require('pg');
+const connectionString = process.env.PG_URL || process.env.PG_URL_DOCKER;
+
+const retryInterval = 2000;
+
+async function wait() {
+  if (!connectionString) {
+    console.error('No PG_URL provided');
+    process.exit(1);
+  }
+
+  const client = new Client({ connectionString });
+  while (true) {
+    try {
+      await client.connect();
+      await client.end();
+      console.log('Postgres is available');
+      process.exit(0);
+    } catch (err) {
+      console.log('Waiting for Postgres...');
+      await new Promise((r) => setTimeout(r, retryInterval));
+    }
+  }
+}
+
+wait();
